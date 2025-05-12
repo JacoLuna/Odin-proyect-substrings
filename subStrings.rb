88dca974dd2a47984e61f3ffe4e2a4345ad221c8
jaco <jacoluna01@gmail.com>
2025-05-11 22:15:19 -0300

    add: expected result

--- subStrings.rb
+++ subStrings.rb
@@ -20,3 +20,4 @@ def substrings(setence, dictionary)
 end
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+#   => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
